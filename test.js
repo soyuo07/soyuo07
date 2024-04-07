d038389eeafbce56bc52585b17d2e2c98ad6b5f2
soyuo07 <166280474+soyuo07@users.noreply.github.com>
2024-04-07 17:24:39 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,16 +1,16 @@
 let data = {
-  REPEAT_COUNT: 10000,
-  CHAR: "Hello, world!"
-}
-
-async function ping() {
-  console.log("test..");
-  let char = "", i = 0, startTime = Date.now();
-  await while(i < data.REPEAT_COUNT) {
-    i++;
-    char += data.CHAR.repeat(i+1);
+    REPEAT_COUNT: 100000,
+    CHAR: "Hello, world!"
   }
-  console.log("Pong! " + (Date.now() - startTime) + "ms");
-}
-
-ping();
+  
+  async function ping() {
+    console.log("test..");
+    let char = "", i = 0, startTime = Date.now();
+    while(i < data.REPEAT_COUNT) {
+      i++;
+      char = data.CHAR.repeat(i+1);
+    }
+    console.log("Pong! " + (Date.now() - startTime) + "ms");
+  }
+  
+  ping();
